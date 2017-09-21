b6827976c040ac5f47bc88284a518f7db38560ad
Adam Friedman <tintoy@tintoy.io>
2017-09-21 11:00:01 +1000

    Remove unused namespace imports.

--- samples/VisualStudioExtension/TextViewCreationListener.cs
+++ samples/VisualStudioExtension/TextViewCreationListener.cs
@@ -2,10 +2,7 @@
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Text;
